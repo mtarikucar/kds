127f9dd7ca34bdf44c6f86cbdfb4d081db1acf59
t00517 <muhammedtarik.ucar@tai.com.tr>
2025-11-20 10:22:42 +0200

    Revert "feat: implement customer online payment system (Phase 1)"
    This reverts commit b89ac808226adbf71833f25f0817861279f5e8f6.

--- backend/src/modules/customer-orders/customer-orders.module.ts
+++ backend/src/modules/customer-orders/customer-orders.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CustomerOrdersService } from './services/customer-orders.service';
-import { CustomerPaymentService } from './services/customer-payment.service';
 import { CustomerOrdersController } from './controllers/customer-orders.controller';
-import { CustomerPaymentController } from './controllers/customer-payment.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { PosSettingsModule } from '../pos-settings/pos-settings.module';
 import { KdsModule } from '../kds/kds.module';
@@ -10,8 +8,8 @@ import { CustomersModule } from '../customers/customers.module';
 
 @Module({
   imports: [PrismaModule, PosSettingsModule, KdsModule, CustomersModule],
-  controllers: [CustomerOrdersController, CustomerPaymentController],
-  providers: [CustomerOrdersService, CustomerPaymentService],
-  exports: [CustomerOrdersService, CustomerPaymentService],
+  controllers: [CustomerOrdersController],
+  providers: [CustomerOrdersService],
+  exports: [CustomerOrdersService],
 })
 export class CustomerOrdersModule {}
